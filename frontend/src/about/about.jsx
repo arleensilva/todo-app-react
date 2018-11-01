c65d7830f86b8fa35509e9fe2709b258071f7806
Arleen Silva <arleensilva20@gmail.com>
2018-11-01 10:00:33 -0300

    Commit inicial

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'/>
+
+        <p>Sobre e é isso ae</p>
+    </div>
+)
